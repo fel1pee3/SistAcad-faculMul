fb4cde284cf0f36fff87adf33de7bb9b0f255f0d
Felipe Maia <132314205+fel1pee3@users.noreply.github.com>
2025-04-06 19:00:03 -0300

    Teste de aceitação: validar fluxo de cadastro de aluno

---
+++ src/test/java/TestAcceptance/AlunoAcceptanceTest.java
@@ -0,0 +1,27 @@
+package TestAcceptance;
+
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.Test;
+import io.restassured.RestAssured;
+
+import static org.hamcrest.Matchers.equalTo;
+
+public class AlunoAcceptanceTest {
+
+    @Test
+    void testCadastro_AlunoComDadosValidos() {
+        String alunoJson = "{ \"nome\": \"Pedro\", \"email\": \"pedro@email.com\"}";
+
+        RestAssured.given()
+                .contentType(ContentType.JSON)
+                .body(alunoJson)
+                .when()
+                .post("http://localhost:8080/alunos/create")
+                .then()
+                .statusCode(200)
+                .body("nome", equalTo("Pedro"))
+                .body("email", equalTo("pedro@email.com"));
+
+    }
+
+}
